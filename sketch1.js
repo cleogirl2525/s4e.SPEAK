d485125cfc7ecb84849019e18bbdbecb006c5786
shawné michaelain holloway <8611937+cleogirl2525@users.noreply.github.com>
2021-10-14 13:10:24 -0400

    Update sketch1.js

--- sketch1.js
+++ sketch1.js
@@ -34,7 +34,7 @@ xxx = loadFont('XXY.otf');
 }
 
 function setup() { 
-  createCanvas(1920, 1080);
+  createCanvas(1620, 680);
   frameRate(2);
 } 
 
@@ -46,7 +46,7 @@ function draw() {
   select('canvas').elt.style.letterSpacing = "20px";
   background(0);
   textAlign(CENTER, CENTER);
-  textSize(340);
+  textSize(250);
   let onScreen = WORDS[i];
   text(onScreen, width/2, height/2);
   i++;
